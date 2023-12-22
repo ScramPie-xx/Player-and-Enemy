6847a2c745223066bdece2290a218466ccb1d702
ScramPie <123657227+ScramPie-xx@users.noreply.github.com>
2023-12-22 13:25:27 +0400

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+
+    public float speed;
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
+    }
+
+}
